f1f3d31d15861a16739d5a04ed8c63eb664270b8
‘Hugo <hugo.garcia@the-cocktail.com>
2023-08-28 16:31:51 +0200

    Added cache storage for movies and seasons

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,7 +1,11 @@
 class MovieService
 
   def self.get_movies_by_descending_creation
-    Movie.includes(:purchase_options).by_descending_creation
+    
+    Rails.cache.fetch('movies_by_descending_creation') do
+      Movie.includes(:purchase_options).by_descending_creation
+    end 
+
   end
 
 end
--- app/services/season_service.rb
+++ app/services/season_service.rb
@@ -2,10 +2,12 @@ class SeasonService
 
   def self.get_seasons_desc_episodes_asc
     
-    Season.eager_load(:episodes).includes(:purchase_options)
-                                .merge(Episode.by_ascending_number)
-                                .by_descending_creation
-
+    Rails.cache.fetch('seasons_desc_episodes_asc') do
+      Season.eager_load(:episodes).includes(:purchase_options)
+                                  .merge(Episode.by_ascending_number)
+                                  .by_descending_creation
+    end
+    
   end
 
 end
