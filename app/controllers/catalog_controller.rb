7918036bfda350fe934446f01c38af21ccec0d14
‘Hugo <hugo.garcia@the-cocktail.com>
2023-08-28 16:40:37 +0200

    Added Catalog index endpoint and catalog service

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,2 +1,16 @@
 class CatalogController < ApplicationController
+
+  def index
+    
+    begin
+      catalog = CatalogService.get_catalog
+      render json: catalog, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: e }, status: :not_found
+    rescue StandardError => e
+      render json: { error: e }, status: :internal_server_error
+    end 
+
+  end
+
 end
---
+++ app/services/catalog_service.rb
@@ -0,0 +1,11 @@
+class CatalogService 
+
+  def self.get_catalog
+
+    movies = MovieService.get_movies_by_descending_creation
+    seasons = SeasonService.get_seasons_desc_episodes_asc
+    catalog = (movies + seasons).sort_by(&:created_at).reverse
+
+  end
+
+end
