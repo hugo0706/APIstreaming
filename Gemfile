a9ff07f937f08152aa8329f15bc648da04a94677
‘Hugo <hugo.garciac@edu.uah.es>
2023-08-30 17:50:31 +0200

    Added purchase endpoint tests

--- Gemfile
+++ Gemfile
@@ -46,6 +46,9 @@ group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem 'rspec-rails'
   gem 'factory_bot_rails'
+  gem 'simplecov'
+  
+  gem 'timecop'
 end
 
 group :development do
--- spec/requests/purchases_spec.rb
+++ spec/requests/purchases_spec.rb
@@ -1,7 +1,140 @@
 require 'rails_helper'
 
 RSpec.describe "Purchases", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /purchase" do
+    let(:movie) { FactoryBot.create(:movie,:with_purchase_options) }
+    let(:user) { FactoryBot.create(:user) }      
+    let(:season) { FactoryBot.create(:season,:with_episodes,:with_purchase_options) }
+
+    let(:expected_keys) do 
+      ['id','user_id','email','title','purchase_option','purchasable_type','purchasable_id','expires_at','created_at','updated_at']
+    end
+    
+    context 'when purchase is a movie' do
+
+      it 'creates a purchase' do
+        params_movie ={
+            "purchase": {
+              "purchasable_type": "Movie",
+              "purchasable_id": movie.id,
+              "user_id": user.id,
+              "purchase_option_id": movie.purchase_options.first.id
+            }
+          }
+        expect{
+          post '/purchase', params: params_movie
+        }.to change(Purchase, :count).by(1)
+      end
+
+      it 'returns a purchased movie and status :created' do
+        params_movie ={
+            "purchase": {
+              "purchasable_type": "Movie",
+              "purchasable_id": movie.id,
+              "user_id": user.id,
+              "purchase_option_id": movie.purchase_options.first.id
+            }
+          }
+
+        post '/purchase', params: params_movie
+        expect(response).to have_http_status(:created)
+        response_json = JSON.parse(response.body)
+        keys = response_json.keys
+        expect(keys).to contain_exactly(*expected_keys)
+        expect(response_json["purchasable_type"]).to eq("Movie")
+      end
+    end
+
+    context 'when purchase is a season' do
+
+      it 'creates a purchase' do
+        params_season ={
+            "purchase": {
+              "purchasable_type": "Season",
+              "purchasable_id": season.id,
+              "user_id": user.id,
+              "purchase_option_id": season.purchase_options.first.id
+            }
+          }
+        expect{
+          post '/purchase', params: params_season
+        }.to change(Purchase, :count).by(1)
+      end
+
+      it 'returns a purchased season and status :created' do
+        params_season ={
+          "purchase": {
+            "purchasable_type": "Season",
+            "purchasable_id": season.id,
+            "user_id": user.id,
+            "purchase_option_id": season.purchase_options.first.id
+          }
+        }
+
+        post '/purchase', params: params_season
+        expect(response).to have_http_status(:created)
+        response_json = JSON.parse(response.body)
+        keys = response_json.keys
+        expect(keys).to contain_exactly(*expected_keys)
+        expect(response_json["purchasable_type"]).to eq("Season")
+      end
+    end
+  
+    context 'when params are incorrect' do
+      it 'returns error status 422' do
+        post '/purchase', params: {}
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to include('error')
+      end
+    end
+  
+    context 'when trying to purchase a movie twice' do
+      it 'returns error Purchase option has already been taken status 422' do
+        params_movie ={
+            "purchase": {
+              "purchasable_type": "Movie",
+              "purchasable_id": movie.id,
+              "user_id": user.id,
+              "purchase_option_id": movie.purchase_options.first.id
+            }
+          }
+
+        post '/purchase', params: params_movie
+        post '/purchase', params: params_movie
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to include('Purchase option has already been taken')
+      end
+    end
+  
+    context 'when trying to purchase a movie twice two days later' do
+      it 'returns a purchased movie and status :created' do
+        params_movie ={
+          "purchase": {
+            "purchasable_type": "Movie",
+            "purchasable_id": movie.id,
+            "user_id": user.id,
+            "purchase_option_id": movie.purchase_options.first.id
+          }
+        }
+        post '/purchase', params: params_movie
+        expect(response).to have_http_status(:created) 
+        response_json = JSON.parse(response.body)
+        keys = response_json.keys
+        expect(keys).to contain_exactly(*expected_keys)
+        expect(response_json["purchasable_type"]).to eq("Movie")
+
+        Timecop.travel(2.days.from_now) do
+          post '/purchase', params: params_movie
+          expect(response).to have_http_status(:created)
+          response_json = JSON.parse(response.body)
+          keys = response_json.keys
+          expect(keys).to contain_exactly(*expected_keys)
+          expect(response_json["purchasable_type"]).to eq("Movie")
+        end
+      end
+    end
   end
+  
 end
